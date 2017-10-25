52b4e34ebf73943d2e56b68075a89033136fcf9e
Dominika Michalkova <dmichalk@sk.ibm.com>
2017-10-25 14:56:35 +0200

    add comments

--- 1.rename.sh
+++ 1.rename.sh
@@ -1,7 +1,6 @@
-#Shell script to rename file names that contain blank spaces and special characters " " and replace them with "-" char
+#script to rename file names that contain blank spaces and special characters " " and replace them with "-" char
 #paste the script in every location your files are and run it from its directory
 #if you are on Win OS, install cygwin and after successful installation run the script on Win cmd
-
 #!/bin/bash
 invalid="[^a-zA-Z_0-9.-]" #regex, ^ inside - negates the character class
 FILES=*
